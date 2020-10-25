42d262ecaeafc5c875aa5e6a19abc2ba142038bc
Viacheslav V Bogdanov <viacheslav.v.bogdanov@gmail.com>
2020-10-25 20:08:56 +0500

    tabs mock

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -3,19 +3,32 @@
     <ion-tabs>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon :icon="planetOutline" />
+          <ion-label>News</ion-label>
         </ion-tab-button>
-          
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
-        
+
+
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon :icon="search" />
+          <ion-label>Shops</ion-label>
         </ion-tab-button>
+
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+          <ion-icon :icon="bagAddOutline" />
+          <ion-label>Sell</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="heartOutline" />
+          <ion-label>Purchases</ion-label>
+        </ion-tab-button>
+
+
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="personCircleOutline" />
+          <ion-label>Storefront</ion-label>
+        </ion-tab-button>
+
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -23,16 +36,14 @@
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { planetOutline, bagAddOutline, search, heartOutline, personCircleOutline } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
     return {
-      ellipse, 
-      square, 
-      triangle,
+      planetOutline, bagAddOutline, search, heartOutline, personCircleOutline
     }
   }
 }
