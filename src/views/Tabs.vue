6d5e8fae478ef50c9d7a95c433b86874e5a8f5bd
Viacheslav Bogdanov <viacheslav.v.bogdanov@gmail.com>
2021-02-08 19:05:28 +0500

    restart

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -3,32 +3,19 @@
     <ion-tabs>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="planetOutline" />
-          <ion-label>News</ion-label>
+          <ion-icon :icon="triangle" />
+          <ion-label>Tab 1</ion-label>
         </ion-tab-button>
-
-
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="search" />
-          <ion-label>Shops</ion-label>
-        </ion-tab-button>
-
+          
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="bagAddOutline" />
-          <ion-label>Sell</ion-label>
+          <ion-icon :icon="ellipse" />
+          <ion-label>Tab 2</ion-label>
         </ion-tab-button>
-
+        
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="heartOutline" />
-          <ion-label>Purchases</ion-label>
+          <ion-icon :icon="square" />
+          <ion-label>Tab 3</ion-label>
         </ion-tab-button>
-
-
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="personCircleOutline" />
-          <ion-label>Storefront</ion-label>
-        </ion-tab-button>
-
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -36,14 +23,16 @@
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { planetOutline, bagAddOutline, search, heartOutline, personCircleOutline } from 'ionicons/icons';
+import { ellipse, square, triangle } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
     return {
-      planetOutline, bagAddOutline, search, heartOutline, personCircleOutline
+      ellipse, 
+      square, 
+      triangle,
     }
   }
 }
